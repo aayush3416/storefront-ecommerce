923202d8a9d87a9c7224f8c1b9d73cff692ca8ee
aayush3416 <87783633+aayush3416@users.noreply.github.com>
2022-11-26 16:21:19 -0500

    handle cart and checkkout page for the logged in users

--- storefront/settings.py
+++ storefront/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1^k^zw&jdgj_(qp8o^*35^9%31z265oj4ems2jgl9-%5)v9ym&'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
