fdddae157fb79741b192f40e5c6e2aac381a0575
aayush3416 <87783633+aayush3416@users.noreply.github.com>
2022-12-10 00:36:50 -0500

    update settings

--- storefront/settings.py
+++ storefront/settings.py
@@ -167,7 +167,7 @@ AWS_DEFAULT_ACL = None
 AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
-    'greatkart/static',
+    'storefront/static',
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
