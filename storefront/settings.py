326c61f57a19561df23802a76d3a07dd1d45ded0
aayush3416 <87783633+aayush3416@users.noreply.github.com>
2022-12-09 19:53:31 -0500

    allowed hosts

--- storefront/settings.py
+++ storefront/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)  # True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'django-storefront-env.eba-ys3de3me.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
